2c0c0b451491419607ba484705593fc04ca50407
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2020-09-18 12:11:53 -0500

    About to deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,72 @@
+/* eslint-disable quotes, indent */
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
 
-app.use(morgan('dev'));
+app.use(function validateBearerToken(req, res, next) {
+    //const bearerToken = req.get('Authorization').split(' ')[1];
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
 
-app.use((req, res) => {
-  res.send('Hello, world! Joey');
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' });
+    }
+    // move to the next middleware
+    next();
 });
 
-const PORT = 8000;
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+function handleGetTypes(req, res) {
+    res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+    let pokemon = POKEDEX.pokemon;
+
+    // filter our pokemon by name if name query param is present
+    if (req.query.name) {
+        pokemon = pokemon.filter(pokemon =>
+            // case insensitive searching
+            pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+        );
+    }
+    // filter our pokemon by type if type query param is present
+    if (req.query.type) {
+        pokemon = pokemon.filter(pokemon =>
+            pokemon.type.includes(req.query.type)
+        );
+    }
+    res.json(pokemon);
+
+}
+
+app.get('/pokemon', handleGetPokemon);
+
+app.use((error, req, res, next) => {
+    let response;
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } };
+    } else {
+        response = { error };
+    }
+    res.status(500).json(response);
+});
+
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-})
+
+});
